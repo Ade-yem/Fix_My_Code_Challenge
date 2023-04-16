7b2fd6924f540338f42b0860480b1850848bcdd5
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-04-16 15:22:27 -0700

    all mandatory tasks completed

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -6,9 +6,14 @@ User class
 class User():
     """ Documentation """
 
-    def __init__(self):
+    def __init__(self, email):
         """ Documentation """
-        self.__email = None
+        self.__email = email
+
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
 
     @email.setter
     def email(self, value):
@@ -17,14 +22,11 @@ class User():
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
    
     
 if __name__ == "__main__":
 
-    u = User()
-    u.email = "john@snow.com"
+    email = "john@snow.com"
+
+    u = User(email)
     print(u.email)
